0cdf75ed6c4e722b547c610f6a872feccc4e9e64
ChengBang Zhou <zhouchengbang@gmail.com>
2018-01-16 01:02:22 +0800

    temp commit

--- dota2bocai_spider.py
+++ dota2bocai_spider.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import bs4
 import requests
 
 def get_html(url):
@@ -11,3 +12,16 @@ def get_html(url):
     except:
         return "ERROR"
 
+def print_reslut(url):
+    html = get_html(url)
+    soup = BeautifulSoup(html)
+    match_list = soup.find_all('div',attrs={'class':'matchmain_bisai_qukuai'})
+    for match in match_list:
+        time = match.find('div',attrs={'class':'whenm'}).text.strip()
+        teamname = match.find_all('span',attrs={'class':'team_name'})
+        if(type(teamname[0].string) == bs4.element.Comment):
+            teamname_1 = '暂无队名'
+        else:
+            teamname_1 = teamname[0].string
+
+        team1_support_level = match.find('span',)
